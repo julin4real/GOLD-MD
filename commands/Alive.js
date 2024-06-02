9b1dbb166e2eaed15409acc96157ac0e3d96c58a
julin4real <128611874+julin4real@users.noreply.github.com>
2024-06-02 15:28:51 -0400

    Update Alive.js

--- commands/Alive.js
+++ commands/Alive.js
@@ -2,14 +2,14 @@
 
 /** 
 
-           • GOLD MD WHATSAPP BOT •
+           • jul4rl instagram MD WHATSAPP BOT •
 
   𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
  𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
  𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
  𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
  * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : GOLD 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
- * @𝗼𝘄𝗻𝗲𝗿: UMAR
+ * @𝗼𝘄𝗻𝗲𝗿: julin
  
  **/
 
@@ -65,7 +65,7 @@ const date = moment().format('DD/MM/YYYY');
  ${message}
  
  
- *🔰 GOLD MD IS ALIVE NOW 🔰*`
+ *🔰 jul4rl MD IS ALIVE NOW 🔰*`
 
  if (lien.match(/\.(mp4|gif)$/i)) {
     try {
@@ -96,7 +96,7 @@ else {
         if(!superUser) { repondre("Bot me koi alive msg set nahi") ; return};
 
       await   repondre("Agar apko bot me alive msg set karna hai to ese likho: .alive message;photo link");
-         repondre("*🔰 HI I AM GOLD MD 🔰*")
+         repondre("*🔰 HI I AM jul4rl instagram MD 🔰*")
      }
  } else {
 
